e1988a3d6771fca99a407b45051c036a8b289cf5
Wanderson <lopez.wanderson@gmail.com>
2022-05-10 16:30:03 -0600

    Initial Commit

---
+++ NavigationViewScrollView/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NavigationViewScrollView
+//
+//  Created by Wanderson López on 10/05/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NavigationViewScrollView/NavigationViewScrollViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationViewScrollViewApp.swift
+//  NavigationViewScrollView
+//
+//  Created by Wanderson López on 10/05/22.
+//
+
+import SwiftUI
+
+@main
+struct NavigationViewScrollViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
